4ab45455a2abf36773afb6dbaa923ae09a7a9b19
Kigo-P <peterkigo33@gmail.com>
2024-08-02 12:53:06 +0300

    last commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,26 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+
+
+function About(props) {
+  console.log(props)
+  // function isBio(){
+  //   if (props.bio === ""|| !props.bio){
+  //     return null
+  //   }else{
+  //     return <p>{props.bio}</p>
+  //   }
+  // }
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {/* <p>{isBio()}</p> */}
+      {props.bio && <p>{props.bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links github= {props.github} linkedin={props.linkedin}/>
+
     </div>
   );
 }
